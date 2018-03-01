3b528fd34b6ce8142f3892887f34bd80df11b5a4
isayan <italosayan@gmail.com>
2018-03-01 10:34:01 -0500

    classes

--- Hello.scala
+++ Hello.scala
@@ -12,10 +12,12 @@ println({
 })
 
 //Function
-val add = (x: Int, y: Int) => Int = x + y
+//Object with apply method.
+val add = (x: Int, y: Int) => x + y
 println(add(1, 2)) // 3
 (x: Int) => x + 1// Anonymous Function
-//Method
+
+//Method: Do not have types
 def add(x: Int, y: Int): Int = x + y
 println(add(1, 2)) // 3
 
@@ -24,10 +26,64 @@ def addThenMultiply(x: Int, y: Int)(multiplier: Int): Int = (x + y) * multiplier
 println(addThenMultiply(1, 2)(3)) // 9
 
 //Function Syntax
-//val name = (varname1:type, ...)(varnameone: type ...)=> What does it return? type = expression
+//val name = (varname1:type, ...)(varnameone: type ...)=> What does it return? expression
 //Method Syntax
 //def name(varname1:type, ...)(varnameone: type ...): What does it return? type = expression
 
 /*
-PARTIAL APPLICATION
+2.0
 */
+
+//Partial Application
+val adder(m: Int, n: Int) = m + n
+val add2 = adder(2, _:Int)
+
+
+//Curried functions
+val curriedAdd = (adder _).curried
+val addTwo = curriedAdd(2)
+addTwo(4)// 6
+
+//Variable length arguments
+def capitalizeAll(args: String*) = {
+  args.map { arg =>
+    arg.capitalize
+  }
+}
+capitalizeAll("rarity", "applejack") // ArrayBuffer(Rarity, Applejack)
+
+//Classes
+class Calculator(brand: String) {
+	def add(m: Int, n: Int): Int = m + n
+	/**
+	A constructor.
+	*/
+	val color: String = if (brand == "TI") {
+		"blue"
+	} else if (brand == "HP") {
+		"black"
+	} else {
+		"white"
+	}
+
+}//defined class Calculator
+
+val calc = new Calculator("HP")
+calc.add(1, 2) //Int = 3
+calc.color//String = black
+
+//Inheritance
+class ScientificCalculator(brand: String) extends Calculator(brand) {
+  def log(m: Double, base: Double) = math.log(m) / math.log(base)
+}
+
+//Traits
+//traits are collections of fields and behaviors that you can extend or mixin to your classes.
+
+trait Car {
+  val brand: String
+}
+
+trait Shiny {
+  val shineRefraction: Int
+}
